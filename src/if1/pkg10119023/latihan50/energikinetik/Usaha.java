539ef0cdf47d178a0df19a5df6abc2c1053e5601
M-Farhan-R <72879545+M-Farhan-R@users.noreply.github.com>
2020-11-15 12:12:10 +0700

    Update Usaha.java

--- src/if1/pkg10119023/latihan50/energikinetik/Usaha.java
+++ src/if1/pkg10119023/latihan50/energikinetik/Usaha.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package if1.pkg10119023.lahitan50.energikinetik;
+package if1.pkg10119023.latihan50.energikinetik;
 
 /**
  *
